fcca872e084ceca194b983a9cff30ffdd01b6893
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-03 18:49:00 +0600

    BFS Algorithms implement

--- BFS_Algorithm.cpp
+++ BFS_Algorithm.cpp
@@ -3,30 +3,37 @@
 #include "Queue.h"
 #include "Queue.cpp" using
 namespace std;
+
 void BFS(int **AdjMat,char vertex[],int u){
 int i,j;
 char color[u];
 char prev[u]; int
 d[u];
+
 for(i=1; i<u; i++){ //root = AdjMat[0]
 color[i] = 'W'; //White = W
-prev[i] = 'N'; //NULL = N d[i] =
-5000; //Inf = 5000
-} color[0]
-= 'G'; d[0] =
-0; prev[0] =
-'N';
+prev[i] = 'N'; //NULL = N d[i] =5000; //Inf = 5000
+} 
+
+color[0]= 'G'; 
+d[0] =0; 
+prev[0] ='N';
+
 Queue<char> Q(u);
 Q.Enqueue(vertex[0]);
-while(!Q.IsEmpty()){
+while(!Q.IsEmpty())
+{
 char p;
 Q.Dequeue(p);
 int k = 0;
 while(vertex[k]!=p){
 k++;
 }
-for(j=0; j<u; j++){
-if(AdjMat[k][j]==1 && color[j]=='W'){
+
+for(j=0; j<u; j++)
+{
+if(AdjMat[k][j]==1 && color[j]=='W')
+{
 color[j] = 'G';
 d[j] = d[k]+1;
 prev[j] = p;
@@ -68,18 +75,27 @@ for(i=0; i<a; i++){
 cin>>vertices[i]; } int
 **AdjMatrix;
 AdjMatrix = new int*[a];
-for(i=0; i<a; i++){
+
+for(i=0; i<a; i++)
+{
 AdjMatrix[i] = new int[a];
-} for(i=0; i<a; i++) {
-for(j=0; j<a; j++) {
+} 
+
+for(i=0; i<a; i++) 
+{
+for(j=0; j<a; j++) 
+{
 AdjMatrix[i][j] = 0;
 }
 }
-for (i=0; i<b; i++) {
+
+for (i=0; i<b; i++) 
+{
 char x,y; int
 m=0,n=0;
 cout<<"Enter edges: ";
 cin>>x>>y;
+
 while(vertices[m]!=x){
 m++;
 }
